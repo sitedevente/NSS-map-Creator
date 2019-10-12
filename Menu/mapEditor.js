dfeb259b88bef2eb21eb52e430c698842a5bca9d
sitedevente <martinjarod17@gmail.com>
2019-10-12 20:32:45 +0200

    Test to edit electron menu

---
+++ Menu/mapEditor.js
@@ -0,0 +1,57 @@
+const template = [
+    {
+        "label": "View",
+        "submenu": [
+            {
+                "label": "Toggle Full Screen",
+                "accelerator": (function () {
+                    if (process.platform == 'darwin')
+                        return 'Ctrl+Command+F';
+                    else
+                        return 'F11';
+                })(),
+                "click": function (item, focusedWindow) {
+                    if (focusedWindow) {
+                        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+                    }
+                }
+            },
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click: function (item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.reload();
+                }
+            },
+            {
+                label: 'Toggle Full Screen',
+                accelerator: (function () {
+                    if (process.platform == 'darwin')
+                        return 'Ctrl+Command+F';
+                    else
+                        return 'F11';
+                })(),
+                click: function (item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+                }
+            },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: (function () {
+                    if (process.platform == 'darwin')
+                        return 'Alt+Command+I';
+                    else
+                        return 'Ctrl+Shift+I';
+                })(),
+                click: function (item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    }
+];
+
+module.exports = template;
