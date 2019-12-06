d769bb62a2f4bdbdd0e37d18c2b672ff883ede89
sitedevente <martinjarod17@gmail.com>
2019-12-06 17:05:51 +0100

    Tile painting feature is back
    Carousel and the tile painting feature are now working as they should be.

--- tile-manager.js
+++ tile-manager.js
@@ -6,55 +6,14 @@ const eraserClickListener = () => {
 	pickedImage = 'eraser';
 }
 
-
 const addEraser = () => {
 	const [eraser] = document.getElementsByClassName('eraser')
 	eraser.addEventListener('click', eraserClickListener)
 }
 
-// Allow user to choose an image to paint on grid's cells
-const imageClickListener = ({currentTarget}) => {
-	pickedImage = currentTarget;
-	console.log(pickedImage)
-};
-
-const createImage = (imagePath) => {
-	const img = document.createElement('img')
-	img.setAttribute('src',imagePath)
-	img.addEventListener('click', imageClickListener)
-	return img
-}
-
-// Allow user to paint on the cliked tile
-const tileClickListener = ({currentTarget}) => {
-	if(pickedImage){
-		if(currentTarget.hasChildNodes()){
-			currentTarget.childNodes[0].remove();
-			console.log('deleted')
-		}
-		if(pickedImage != 'eraser'){
-			currentTarget.appendChild(pickedImage.cloneNode(true))
-			console.log('added')
-		}
-	}
-	console.log('finished')
-};
-
-const createCell = () => {
-	const cell = document.createElement('div')
-	cell.setAttribute('class','cell')
-	cell.addEventListener('click', tileClickListener)
-	return cell
-}
-
-const drawTileManager = () => {
-	const [grid] = $('#tile-manager');
-	const gridSize = 32*18;
-	for(let cellNumber = 0 ; cellNumber < gridSize ; ++cellNumber){
-		grid.appendChild(createCell())
-	}
-}
-
-$(document).ready(
-    drawTileManager()
-)
+// const createImage = (imagePath) => {
+// 	const img = document.createElement('img')
+// 	img.setAttribute('src',imagePath)
+// 	img.addEventListener('click', imageClickListener)
+// 	return img
+// }
--- tileCarousel.js
+++ tileCarousel.js
@@ -1,6 +1,15 @@
 $(document).ready(() => {
+    // Image you want to paint on tiles
+    let pickedImage = null;
+    
+    $('.tooltipped').tooltip()
 
-    // function keyPress(keyCode) { if (Element. == ) { }}
+    // Allow user to choose an image to paint on grid's cells
+    const imageClickListener = ({currentTarget}) => {
+        console.log('hihi')
+        pickedImage = $(currentTarget).parent().parent().find('.card-image img')[0]
+        console.log(pickedImage)
+    };
 
     function initCarousel() {
         $('#tileCarousel').carousel({
@@ -8,14 +17,49 @@ $(document).ready(() => {
             padding: 90,
             shift: 100
         })
+
+        $('.btn.green.right').click(imageClickListener);
     }
+
+    const createCell = () => {
+        const cell = document.createElement('div')
+        cell.setAttribute('class', 'cell')
+        cell.addEventListener('click', tileClickListener)
+        return cell
+    }
+
+    
+    const drawTileManager = () => {
+        const [grid] = $('#tile-manager');
+        const gridSize = 32 * 18;
+        for (let cellNumber = 0; cellNumber < gridSize; ++cellNumber) {
+            grid.appendChild(createCell())
+        }
+    }
+
+    drawTileManager();
+
+    // Allow user to paint on the cliked tile
+    function tileClickListener ({currentTarget}) {
+        if(pickedImage){
+            if(currentTarget.hasChildNodes()){
+                currentTarget.childNodes[0].remove();
+                console.log('deleted')
+            }
+            if(pickedImage != 'eraser'){
+                currentTarget.appendChild(pickedImage.cloneNode(true))
+                console.log('added')
+            }
+        }
+        console.log('finished')
+    };
+
+    //$('.cell').click(tileClickListener)
 
     $('#tilesets').modal({
         dismissible: false,
         'onOpenEnd': initCarousel
     })
-
-    $('.tooltipped').tooltip()
 
 
 });
